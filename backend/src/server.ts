f84962fc92fe7481b3326a0a1eab7fdbe0bc4eca
Adam Lin <linweizhe94@qq.com>
2025-06-14 02:53:59 +0800

    update project structure

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+// 处理/api请求
+app.get('/api', (req, res) => {
+  res.send('这是来自/api的响应');
+});
+
+// 代理Vue静态文件
+app.use(express.static(path.resolve(process.cwd(), '../frontend/dist')));
+
+app.listen(port, () => {
+  console.log(`服务器运行在 http://localhost:${port}`);
+});
