6bc32aaa5837899e25be50c86e3325523bd3463b
Adam Lin <linweizhe94@qq.com>
2025-06-14 21:43:21 +0800

    use scss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   server: {
     proxy: {
       '/api': {
@@ -13,4 +19,11 @@ export default defineConfig({
       },
     },
   },
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/_variables.scss" as *;`,
+      },
+    },
+  },
+});
