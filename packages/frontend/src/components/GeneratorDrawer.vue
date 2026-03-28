74a7f009b3f60e62a2fa21d38f4b54845c3735c1
Adam Lin <linweizhe94@qq.com>
2026-03-28 22:49:28 +0800

    refactor(GeneratorDrawer, GeneratorPage): improve code formatting and consistency

--- packages/frontend/src/components/GeneratorDrawer.vue
+++ packages/frontend/src/components/GeneratorDrawer.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
-import { type DeepReadonly, reactive } from 'vue'
-import MonacoEditor from '@components/MonacoEditor.vue'
-import * as monaco from 'monaco-editor'
-import singboxSchema from '@/schemas/sing-box.schema.json'
-import { useFilterStore, useGeneratorStore } from '@/stores.ts'
-import { storeToRefs } from 'pinia'
-import { ElMessage } from 'element-plus'
-import type { Generator } from '@psc/common'
-import { GeneratorCreateOrUpdateSchema } from '@psc/common'
+import { type DeepReadonly, reactive } from "vue";
+import MonacoEditor from "@components/MonacoEditor.vue";
+import * as monaco from "monaco-editor";
+import singboxSchema from "@/schemas/sing-box.schema.json";
+import { useFilterStore, useGeneratorStore } from "@/stores.ts";
+import { storeToRefs } from "pinia";
+import { ElMessage } from "element-plus";
+import type { Generator } from "@psc/common";
+import { GeneratorCreateOrUpdateSchema, getErrorMessage } from "@psc/common";
 
-const filterStore = useFilterStore()
-const filterStoreRefs = storeToRefs(filterStore)
-const filters = filterStoreRefs.filters
-const subscriptionGeneratorStore = useGeneratorStore()
+const filterStore = useFilterStore();
+const filterStoreRefs = storeToRefs(filterStore);
+const filters = filterStoreRefs.filters;
+const subscriptionGeneratorStore = useGeneratorStore();
 
 const { toUpdateGenerator = undefined } = defineProps<{
-    toUpdateGenerator?: DeepReadonly<Generator>
-}>()
+    toUpdateGenerator?: DeepReadonly<Generator>;
+}>();
 
 const subscriptionGenerator = reactive({
-    name: '',
+    name: "",
     filterIds: [] as number[],
-    type: 'url',
-    url: '',
-    content: '',
-})
+    type: "url",
+    url: "",
+    content: "",
+});
 
-const visible = defineModel<boolean>('visible')
+const visible = defineModel<boolean>("visible");
 
 function onEditorMount() {
     monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
@@ -34,33 +34,33 @@ function onEditorMount() {
         allowComments: true,
         schemas: [
             {
-                uri: 'https://gist.githubusercontent.com/artiga033/fea992d95ad44dc8d024b229223b1002/raw/83c676c1ec9f37af2bce0505da396b5444b30032/sing-box.schema.json',
-                fileMatch: ['*'],
+                uri: "https://gist.githubusercontent.com/artiga033/fea992d95ad44dc8d024b229223b1002/raw/83c676c1ec9f37af2bce0505da396b5444b30032/sing-box.schema.json",
+                fileMatch: ["*"],
                 schema: singboxSchema,
             },
         ],
-    })
+    });
 }
 
 function onOpen() {
-    subscriptionGenerator.name = ''
-    subscriptionGenerator.filterIds = []
-    subscriptionGenerator.type = 'url'
-    subscriptionGenerator.url = ''
-    subscriptionGenerator.content = ''
+    subscriptionGenerator.name = "";
+    subscriptionGenerator.filterIds = [];
+    subscriptionGenerator.type = "url";
+    subscriptionGenerator.url = "";
+    subscriptionGenerator.content = "";
     if (toUpdateGenerator) {
-        subscriptionGenerator.name = toUpdateGenerator.name
-        subscriptionGenerator.filterIds = toUpdateGenerator.filterIds.map((id) => id)
-        subscriptionGenerator.type = toUpdateGenerator.type
+        subscriptionGenerator.name = toUpdateGenerator.name;
+        subscriptionGenerator.filterIds = toUpdateGenerator.filterIds.map(id => id);
+        subscriptionGenerator.type = toUpdateGenerator.type;
         switch (toUpdateGenerator.type) {
-            case 'url':
-                subscriptionGenerator.url = toUpdateGenerator.url
-                subscriptionGenerator.content = ''
-                break
-            case 'json':
-                subscriptionGenerator.url = ''
-                subscriptionGenerator.content = JSON.stringify(toUpdateGenerator.content, null, 4)
-                break
+            case "url":
+                subscriptionGenerator.url = toUpdateGenerator.url;
+                subscriptionGenerator.content = "";
+                break;
+            case "json":
+                subscriptionGenerator.url = "";
+                subscriptionGenerator.content = JSON.stringify(toUpdateGenerator.content, null, 4);
+                break;
         }
     }
 }
@@ -68,33 +68,31 @@ function onOpen() {
 function validateJson() {
     if (subscriptionGenerator.content) {
         try {
-            JSON.parse(subscriptionGenerator.content)
+            JSON.parse(subscriptionGenerator.content);
         } catch (e) {
-            ElMessage.error(`无效的JSON:${getErrorMessage(e)}`)
+            ElMessage.error(`无效的JSON:${getErrorMessage(e)}`);
         }
     } else {
-        ElMessage.error('请输入JSON内容')
+        ElMessage.error("请输入JSON内容");
     }
 }
 
 function onConfirm() {
-    if (subscriptionGenerator.type === 'json') {
-        validateJson()
+    if (subscriptionGenerator.type === "json") {
+        validateJson();
     }
 
-    const { data, error, success } = GeneratorCreateOrUpdateSchema.safeParse(subscriptionGenerator)
+    const { data, error, success } = GeneratorCreateOrUpdateSchema.safeParse(subscriptionGenerator);
     if (!success) {
-        ElMessage.error(error.message)
-        return
+        ElMessage.error(error.message);
+        return;
     }
-    const update = toUpdateGenerator
-        ? subscriptionGeneratorStore.updateGenerator(toUpdateGenerator.id, data)
-        : subscriptionGeneratorStore.createGenerator(data)
+    const update = toUpdateGenerator ? subscriptionGeneratorStore.updateGenerator(toUpdateGenerator.id, data) : subscriptionGeneratorStore.createGenerator(data);
     update
         .then(() => (visible.value = false))
-        .catch((err) => {
-            ElMessage.error(err.message)
-        })
+        .catch(err => {
+            ElMessage.error(err.message);
+        });
 }
 </script>
 
@@ -109,17 +107,8 @@ function onConfirm() {
                     <el-input placeholder="请输入订阅名称" v-model="subscriptionGenerator.name" />
                 </el-form-item>
                 <el-form-item>
-                    <el-select
-                        v-model="subscriptionGenerator.filterIds"
-                        multiple
-                        placeholder="Select"
-                    >
-                        <el-option
-                            v-for="item in filters"
-                            :key="item.id"
-                            :label="item.tag"
-                            :value="item.id"
-                        />
+                    <el-select v-model="subscriptionGenerator.filterIds" multiple placeholder="Select">
+                        <el-option v-for="item in filters" :key="item.id" :label="item.tag" :value="item.id" />
                     </el-select>
                 </el-form-item>
                 <el-form-item>
@@ -129,25 +118,11 @@ function onConfirm() {
                     </el-select>
                 </el-form-item>
                 <el-form-item v-show="subscriptionGenerator.type === 'url'">
-                    <el-input
-                        placeholder="请输入模板链接"
-                        v-model="subscriptionGenerator.url"
-                    ></el-input>
+                    <el-input placeholder="请输入模板链接" v-model="subscriptionGenerator.url"></el-input>
                 </el-form-item>
             </el-form>
-            <div
-                v-show="subscriptionGenerator.type === 'json'"
-                class="template-content"
-                style="flex: 1"
-            >
-                <MonacoEditor
-                    v-model="subscriptionGenerator.content"
-                    language="json"
-                    theme="vs-dark"
-                    height="100%"
-                    width="100%"
-                    @mount="onEditorMount"
-                />
+            <div v-show="subscriptionGenerator.type === 'json'" class="template-content" style="flex: 1">
+                <MonacoEditor v-model="subscriptionGenerator.content" language="json" theme="vs-dark" height="100%" width="100%" @mount="onEditorMount" />
             </div>
         </div>
         <template #footer>
--- packages/frontend/src/pages/GeneratorPage.vue
+++ packages/frontend/src/pages/GeneratorPage.vue
@@ -1,69 +1,83 @@
 <script setup lang="ts">
-import {type DeepReadonly, onMounted, ref} from "vue";
+import { type DeepReadonly, onMounted, ref } from "vue";
 import GeneratorDrawer from "@components/GeneratorDrawer.vue";
-import {useGeneratorStore} from "@/stores.ts";
-import {ElMessage} from "element-plus";
-import {storeToRefs} from "pinia";
-import type {Generator} from "@psc/common";
-import {Delete, Edit, View} from '@element-plus/icons-vue'
+import { useGeneratorStore } from "@/stores.ts";
+import { ElMessage } from "element-plus";
+import { storeToRefs } from "pinia";
+import type { Generator } from "@psc/common";
+import { getErrorMessage } from "@psc/common";
+import { Delete, Edit, View } from "@element-plus/icons-vue";
 import MonacoEditor from "@components/MonacoEditor.vue";
 
 const drawerVisible = ref(false);
 const previewVisible = ref(false);
 const previewContent = ref("");
 const subscriptionGeneratorStore = useGeneratorStore();
-const {generators} = storeToRefs(subscriptionGeneratorStore);
+const { generators } = storeToRefs(subscriptionGeneratorStore);
 onMounted(() => {
-  subscriptionGeneratorStore.forceReloadGenerators()
-      .catch(err => {
+    subscriptionGeneratorStore.forceReloadGenerators().catch(err => {
         ElMessage.error(getErrorMessage(err.message));
-      });
-})
+    });
+});
 
 const selectedGenerator = ref<DeepReadonly<Generator> | undefined>(undefined);
 
 function preview(id: number) {
-  subscriptionGeneratorStore.generate(id)
-      .then(content => {
-        previewContent.value = JSON.stringify(content, null, 4);
-        previewVisible.value = true;
-      })
-      .catch(err => ElMessage.error(getErrorMessage(err.message)));
-
+    subscriptionGeneratorStore
+        .generate(id)
+        .then(content => {
+            previewContent.value = JSON.stringify(content, null, 4);
+            previewVisible.value = true;
+        })
+        .catch(err => ElMessage.error(getErrorMessage(err.message)));
 }
-
 </script>
 
 <template>
-  <el-row justify="end">
-    <el-button type="primary" @click="selectedGenerator = undefined; drawerVisible = true">添加订阅</el-button>
-  </el-row>
-  <el-divider/>
-  <el-row>
-    <el-card v-for="item in generators">
-      <template #header>
-        <span>{{ item.name }}</span>
-      </template>
-      <template #footer>
-        <el-button @click="preview(item.id)" type="primary" :icon="View"/>
-        <el-button @click="selectedGenerator = item; drawerVisible = true" type="primary" :icon="Edit"/>
-        <el-button @click="subscriptionGeneratorStore.deleteGenerator(item.id)" type="primary" :icon="Delete"/>
-      </template>
-    </el-card>
-  </el-row>
-  <GeneratorDrawer v-model:visible="drawerVisible" :to-update-subscription-generator="selectedGenerator"/>
-  <el-dialog v-model="previewVisible" width="50vh">
-    <template #header>预览</template>
-    <div style="height: 50vh">
-      <MonacoEditor v-model="previewContent" language="json" :readonly="true" width="100%" height="100%" theme="dark"/>
-    </div>
-  </el-dialog>
+    <el-row justify="end">
+        <el-button
+            type="primary"
+            @click="
+                selectedGenerator = undefined;
+                drawerVisible = true;
+            "
+        >
+            添加订阅
+        </el-button>
+    </el-row>
+    <el-divider />
+    <el-row>
+        <el-card v-for="item in generators">
+            <template #header>
+                <span>{{ item.name }}</span>
+            </template>
+            <template #footer>
+                <el-button @click="preview(item.id)" type="primary" :icon="View" />
+                <el-button
+                    @click="
+                        selectedGenerator = item;
+                        drawerVisible = true;
+                    "
+                    type="primary"
+                    :icon="Edit"
+                />
+                <el-button @click="subscriptionGeneratorStore.deleteGenerator(item.id)" type="primary" :icon="Delete" />
+            </template>
+        </el-card>
+    </el-row>
+    <GeneratorDrawer v-model:visible="drawerVisible" :to-update-subscription-generator="selectedGenerator" />
+    <el-dialog v-model="previewVisible" width="50vh">
+        <template #header>预览</template>
+        <div style="height: 50vh">
+            <MonacoEditor v-model="previewContent" language="json" :readonly="true" width="100%" height="100%" theme="dark" />
+        </div>
+    </el-dialog>
 </template>
 
 <style scoped lang="scss">
 .el-card {
-  width: 300px;
-  height: 300px;
-  margin: 10px;
+    width: 300px;
+    height: 300px;
+    margin: 10px;
 }
 </style>
